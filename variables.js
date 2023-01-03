f09921ab3ff2c43c5d0fa59be79214e074790590
ArmandoVegaLopez <armandovega000@yahoo.com>
2023-01-03 17:31:59 -0600

    Realice cambios en cuanto a la galeria, aplique de manera correcta el diseño responsivo

---
+++ variables.js
@@ -0,0 +1,39 @@
+// //Globales
+// var saludo = "Hola mundo";
+// if (saludo){
+//     var saludo = "Hola China";
+//     console.log(saludo);
+// }
+
+// //Limitadas
+// let nombre ="Ricardo";
+// if(nombre){
+//     let nombre = "Pedro";
+//     console.log(nombre);
+// }
+// console.log(nombre);
+
+// //Inmutables
+// const apellido = "Perez";
+// console.log(apellido);
+
+//String
+let nombre = "Pedro";
+
+//Number
+let edad = 35;
+
+//Boolean
+let verdadero = true;
+let falso = false;
+
+//Undefined
+let indefinido = undefined;
+
+//Null
+let nulo = null;
+
+//Concatenar con interpolaracion
+console.log ('Hola mundo, mi nombre es ' +nombre+ ' y tengo ' +edad+ ' años de edad')
+
+console.log (`Hola mundo, mi nombre es ${nombre} y tengo ${edad} años de edad`);
